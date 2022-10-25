75b56e7221579c760f917fe83d9661af0ab27a77
Amaywe <117047261+Amaywe@users.noreply.github.com>
2022-10-25 14:22:12 +0200

    Initial Commit

---
+++ AFP4/AFP4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFP4App.swift
+//  AFP4
+//
+//  Created by Klesya Loha on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AFP4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
